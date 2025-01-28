a55e0e330e4d0ed28f66fa99293ff1cb80b70216
Ritesh1126 <139070901+Ritesh1126@users.noreply.github.com>
2025-01-28 21:58:50 +0000

    config changes done

---
+++ .devcontainer/01_basics/01_Datatype.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(11 + 26) // We are using nodejs , not browser
+
+console.log(2+2) // code readiability that should be high
+
+console.log('Ritesh')
---
+++ .devcontainer/01_basics/01_Variables.js
@@ -0,0 +1,20 @@
+const account_Id = 112603
+let account_Email = "sontakkeritesh0@gmail.com"
+var account_password = "1234567"
+account_City = "Nagpur"
+account_state = ""
+
+//account_Id = 2 // not allowed
+
+/*
+prefed not use to var 
+because of issue in block scope and functional scope
+*/
+
+account_Email = "SrS@gmail.com"
+account_password = "233234"
+account_City = "chandrapur"
+
+console.log(account_Id);
+
+console.table([account_Id, account_Email, account_password, account_City, account_state])
