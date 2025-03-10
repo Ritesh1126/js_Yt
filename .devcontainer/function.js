7fd25794a6e259c91b301f3e8817ccb4a0558622
RITESH S.SONTAKKE <139070901+Ritesh1126@users.noreply.github.com>
2025-03-10 20:28:52 +0000

    start back to learning

---
+++ .devcontainer/function.js
@@ -0,0 +1,13 @@
+function greet(){
+    console.log("Hi " + user1.name + " my age is " + user1.age + " and my gender is " + user1.gender); // this + sign denotes concatinate function means addition of all stings and values 
+    
+}
+
+
+let user1 = {
+    name : "Ritesh",
+    age : 21,
+    gender : "male"
+}
+
+greet (user1);
