c37f359c0a049dd088dd20a7785a82a3fa5a6ff5
RITESH S.SONTAKKE <139070901+Ritesh1126@users.noreply.github.com>
2025-03-16 19:12:39 +0000

    started learning of array

---
+++ .devcontainer/array.js
@@ -0,0 +1,13 @@
+
+
+let arr = ["Ritesh" , 21 ,{
+    name : "Ritesh",
+    age : 21,
+    cities : ["nagpur" , "Chandrapur" , {
+        state : "maharashtra",
+        country : "india" ,
+        born : 1947
+    }]
+}];
+
+console.log(arr[2].cities[2]);
