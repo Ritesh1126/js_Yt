1f4e9cc619741753ae1fdf024dea90a1c6073132
RITESH S.SONTAKKE <139070901+Ritesh1126@users.noreply.github.com>
2026-02-02 17:59:18 +0000

    Datatypes

---
+++ .devcontainer/data_type.js
@@ -0,0 +1,17 @@
+//There are two type of data types 
+
+// 1. Primitive  --> 7 Types : String , Boolean , null , undefined , symbol , BigInt
+// 2. Non Primitive (Reference ) --> Array , Objects , Functions 
+
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn =  false 
+const outsideTemp = null
+
+let userEmail;
+const id = Symbol('123')
+const anotherID = Symbol('123')
+console.log(id === anotherID);
+
